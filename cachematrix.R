f6505e44e03da4f12c30874ebb6caca386db224d
wownob <wownob@users.noreply.github.com>
2015-08-18 23:23:38 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -35,13 +35,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## so there is no check for this possible failure. 
 #
 cacheSolve <- function(x, ...) {
-  invs <- x$getinv()     # retrieve cached value of inverse
+  invs <- x$getinv()        # retrieve cached value of inverse
   if(!is.null(invs)) {
     message("getting cached data")
-    return(invs)         # cached value is valid -> return it
+    return(invs)            # cached value is valid -> return it
   }
-  data <- x$get()        # retrieve cached value of matrix
-  invs <- solve(data)    # compute inverse
-  x$setinv(invs)         # cache new value
-  invs                   # return new value
+  data <- x$get()           # retrieve cached value of matrix
+  invs <- solve(data, ...)  # compute inverse
+  x$setinv(invs)            # cache new value
+  invs                      # return new value
 }
